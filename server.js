602c009258be9be16ff4afc0e0185b4f02d09398
jhonny <jhonny@pop-os.localdomain>
2021-07-26 13:35:49 -0500

    primer commit

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", function (req, res) {
+  const f = req.files.upfile;
+  const file = {
+    name: f.name,
+    type: f.mimetype,
+    size: f.size,
+  };
 
-
+  res.json(file);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
